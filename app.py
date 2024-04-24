615e67d1cb8eb5bcbddfb330b590d8596f4e4b40
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2024-04-24 16:22:30 +0300

    Serialize the similarity object

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 def recommend(movie):
     movie_index = movies [movies['original_title'] == movie].index[0]
     distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:10]
 
     recommended_movies = []
     for i in movie_list:
